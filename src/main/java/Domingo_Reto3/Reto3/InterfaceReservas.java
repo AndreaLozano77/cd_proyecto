09cbf1ad1267f0e9bddb94acd09cf24f90dcd04e
Andrea Lozano Cataño <andrea.lozano.mt@correo.usa.edu.co>
2021-10-29 20:00:58 -0500

    actualizando backend

--- src/main/java/Domingo_Reto3/Reto3/InterfaceReservas.java
+++ src/main/java/Domingo_Reto3/Reto3/InterfaceReservas.java
@@ -1,6 +1,9 @@
 
 package Domingo_Reto3.Reto3;
 
+import java.util.Date;
+import java.util.List;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 /**
@@ -8,5 +11,11 @@ import org.springframework.data.repository.CrudRepository;
  * @author Andrea
  */
 public interface InterfaceReservas extends CrudRepository<Reservas,Integer>{
+    public List<Reservas> findAllByStatus(String status);
     
+    public List<Reservas> findAllByStartDateAfterAndStartDateBefore(Date dateOne, Date dateTwo);
+    
+    // select clientId, count(*) as "total" from reservacion group by cliendId order by total desc;
+    @Query ("SELECT c.client, COUNT(c.client) from Reservas AS c group by c.client order by COUNT(c.client)DESC")
+    public List<Object[]> countTotalReservationsByClient();
 }
--- src/main/java/Domingo_Reto3/Reto3/ServiciosReservas.java
+++ src/main/java/Domingo_Reto3/Reto3/ServiciosReservas.java
@@ -1,6 +1,10 @@
 
 package Domingo_Reto3.Reto3;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -66,4 +70,32 @@ public class ServiciosReservas {
         }).orElse(false);
         return aBoolean;
     }
+    
+    public StatusReservas getReporteStatusReservaciones(){
+        List<Reservas>completed= metodosCrud.ReservacionStatus("completed");
+        List<Reservas>cancelled= metodosCrud.ReservacionStatus("cancelled");
+        return new StatusReservas(completed.size(), cancelled.size());
+    }
+    
+    public List<Reservas> getReportesTiempoReservaciones(String datoA, String datoB){
+        SimpleDateFormat parser=new SimpleDateFormat ("yyyy-MM-dd");
+        Date datoUno = new Date();
+        Date datoDos = new Date();
+        
+        try{
+            datoUno = parser.parse(datoA);
+            datoDos = parser.parse(datoB);
+        }catch(ParseException evt){
+            evt.printStackTrace();
+        }if(datoUno.before(datoDos)){
+            return metodosCrud.ReservacionTiempo(datoUno, datoDos);
+        }else{
+            return new ArrayList<>();
+        }
+    }  
+    
+    public List<ContadorClientes> servicioTopClientes(){
+        return metodosCrud.getTopClientes();
+    }
+    
 }
